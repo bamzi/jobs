4563452a667b5150710fb36b46c2e0799e60612b
Greg Lazarev <greg@thoughtbot.com>
2013-10-11 14:47:29 -0700

    Add post model

--- app/controllers/posts.go
+++ app/controllers/posts.go
@@ -1,13 +1,29 @@
 package controllers
 
-import "github.com/robfig/revel"
+import(
+  "github.com/robfig/revel"
+  "jobs/app/models"
+)
 
 type Posts struct {
 	*revel.Controller
 }
 
 func (c Posts) Index() revel.Result {
-	return c.Render()
+  results, err := Db.Select(
+    models.Post{},
+    `select * from posts where active = ?`, true)
+  if err != nil {
+    panic(err)
+  }
+
+  var posts []*models.Post
+  for _, r := range results {
+    post := r.(*models.Post)
+    posts = append(posts, post)
+  }
+
+  return c.Render(posts)
 }
 
 func (c Posts) New() revel.Result {
---
+++ app/models/post.go
@@ -0,0 +1,25 @@
+package models
+
+import "github.com/robfig/revel"
+
+type Post struct {
+  Title string
+  Body string
+  Active bool
+}
+
+func (post *Post) Validate(v *revel.Validation) {
+  v.Check(
+    post.Title,
+    revel.Required{},
+    revel.MinSize{3},
+    revel.MaxSize{256},
+  )
+
+  v.Check(
+    post.Body,
+    revel.Required{},
+    revel.MinSize{10},
+    revel.MaxSize{2048},
+  )
+}
